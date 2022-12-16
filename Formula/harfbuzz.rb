ac42c6c72f669bd1af906025940a42a463dd73a4
Rui Chen <rui@chenrui.dev>
2022-12-16 17:21:22 -0500

    harfbuzz 6.0.0

--- Formula/harfbuzz.rb
+++ Formula/harfbuzz.rb
@@ -1,8 +1,8 @@
 class Harfbuzz < Formula
   desc "OpenType text shaping engine"
   homepage "https://github.com/harfbuzz/harfbuzz"
-  url "https://github.com/harfbuzz/harfbuzz/archive/5.3.1.tar.gz"
-  sha256 "77c8c903f4539b050a6d3a5be79705c7ccf7b1cb66d68152a651486e261edbd2"
+  url "https://github.com/harfbuzz/harfbuzz/archive/6.0.0.tar.gz"
+  sha256 "6d753948587db3c7c3ba8cc4f8e6bf83f5c448d2591a9f7ec306467f3a4fe4fa"
   license "MIT"
   head "https://github.com/harfbuzz/harfbuzz.git", branch: "main"
 
