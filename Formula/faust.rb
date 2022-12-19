932d699eb7378dff4949372567f3e0765985064f
Rui Chen <rui@chenrui.dev>
2022-12-19 17:00:41 -0500

    faust 2.54.9

--- Formula/faust.rb
+++ Formula/faust.rb
@@ -1,8 +1,8 @@
 class Faust < Formula
   desc "Functional programming language for real time signal processing"
   homepage "https://faust.grame.fr"
-  url "https://github.com/grame-cncm/faust/releases/download/2.50.6/faust-2.50.6.tar.gz"
-  sha256 "d8b7a89d82ee5d3259c8194c363296bc13353160f847661ab84b2dbefdee7173"
+  url "https://github.com/grame-cncm/faust/releases/download/2.54.9/faust-2.54.9.tar.gz"
+  sha256 "29cfb88f87fd93a55620c18f58ec585a31b6f8106a9fd3528db8340048adef28"
   license "GPL-2.0-or-later"
 
   bottle do
