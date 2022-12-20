18979496df53450c5c219f464655b2046308c8d4
Rui Chen <rui@chenrui.dev>
2022-12-20 10:15:58 -0500

    cargo-c 0.9.15

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -1,8 +1,8 @@
 class CargoC < Formula
   desc "Helper program to build and install c-like libraries"
   homepage "https://github.com/lu-zero/cargo-c"
-  url "https://github.com/lu-zero/cargo-c/archive/v0.9.14.tar.gz"
-  sha256 "d79c12eae1460803a1ce8b440ae213dc4df63a6f2bf39ebd49eea1d7a008bec6"
+  url "https://github.com/lu-zero/cargo-c/archive/v0.9.15.tar.gz"
+  sha256 "2205170f30fe49da05f42b1c151b04042547d60a4429815935a2b6e1069a27bb"
   license "MIT"
 
   livecheck do
