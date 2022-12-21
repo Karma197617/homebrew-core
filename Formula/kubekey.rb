43e8851c11ff363865b7c47a2aacce8c42862dd4
Porkepix <porkepix@gmail.com>
2022-12-21 16:29:12 +0100

    kubekey 3.0.5

--- Formula/kubekey.rb
+++ Formula/kubekey.rb
@@ -2,8 +2,8 @@ class Kubekey < Formula
   desc "Installer for Kubernetes and / or KubeSphere, and related cloud-native add-ons"
   homepage "https://kubesphere.io"
   url "https://github.com/kubesphere/kubekey.git",
-      tag:      "v3.0.4",
-      revision: "f34bf693b4c1770a9fd51c58b5c1ad85e9664f95"
+      tag:      "v3.0.5",
+      revision: "5efdd04e3e4976f3a33081dadef8d4beb1368905"
   license "Apache-2.0"
   head "https://github.com/kubesphere/kubekey.git", branch: "master"
 
