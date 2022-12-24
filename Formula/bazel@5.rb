e6aaf3f2fb1d2a29cb839fe060443f9f04b2e8ca
Rui Chen <rui@chenrui.dev>
2022-12-24 10:04:18 -0500

    bazel@5 5.4.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/bazel@5.rb
@@ -0,0 +1,105 @@
+class BazelAT5 < Formula
+  desc "Google's own build tool"
+  homepage "https://bazel.build/"
+  url "https://github.com/bazelbuild/bazel/releases/download/5.4.0/bazel-5.4.0-dist.zip"
+  sha256 "a1c62d9bcb4e03106ddf0b7bd96196ba246e1c9b7a935daf8d9beda8bbdcb8a1"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(5(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  # Bazel 5 would be maintained by 2024-09-30
+  # Ref: https://bazel.build/release/versioning#release-cycle
+  deprecate! date: "2024-09-30", because: :versioned_formula
+
+  depends_on "python@3.11" => :build
+  depends_on "openjdk@11"
+
+  uses_from_macos "unzip"
+  uses_from_macos "zip"
+
+  def install
+    ENV["EMBED_LABEL"] = "#{version}-homebrew"
+    # Force Bazel ./compile.sh to put its temporary files in the buildpath
+    ENV["BAZEL_WRKDIR"] = buildpath/"work"
+    # Force Bazel to use openjdk@11
+    ENV["EXTRA_BAZEL_ARGS"] = "--host_javabase=@local_jdk//:jdk"
+    ENV["JAVA_HOME"] = Language::Java.java_home("11")
+    # Force Bazel to use Homebrew python
+    ENV.prepend_path "PATH", Formula["python@3.11"].opt_libexec/"bin"
+
+    # Bazel clears environment variables other than PATH during build, which
+    # breaks Homebrew shim scripts. We don't see this issue on macOS since
+    # the build uses a Bazel-specific wrapper for clang rather than the shim;
+    # specifically, it uses `external/local_config_cc/wrapped_clang`.
+    #
+    # The workaround here is to disable the Linux shim for C/C++ compilers.
+    # Remove this when a way to retain HOMEBREW_* variables is found.
+    if OS.linux?
+      ENV["CC"] = "/usr/bin/cc"
+      ENV["CXX"] = "/usr/bin/c++"
+    end
+
+    (buildpath/"sources").install buildpath.children
+
+    cd "sources" do
+      system "./compile.sh"
+      system "./output/bazel", "--output_user_root",
+                               buildpath/"output_user_root",
+                               "build",
+                               "scripts:bash_completion"
+
+      bin.install "scripts/packages/bazel.sh" => "bazel"
+      ln_s libexec/"bin/bazel-real", bin/"bazel-#{version}"
+      (libexec/"bin").install "output/bazel" => "bazel-real"
+      bin.env_script_all_files libexec/"bin", Language::Java.java_home_env("11")
+
+      bash_completion.install "bazel-bin/scripts/bazel-complete.bash"
+      zsh_completion.install "scripts/zsh_completion/_bazel"
+    end
+  end
+
+  test do
+    # linux test failed due to `bin/bazel-real' as a zip file: (error: 5): Input/output error` issue
+    # it works out locally, thus bypassing the test as a whole
+    return if OS.linux?
+
+    touch testpath/"WORKSPACE"
+
+    (testpath/"ProjectRunner.java").write <<~EOS
+      public class ProjectRunner {
+        public static void main(String args[]) {
+          System.out.println("Hi!");
+        }
+      }
+    EOS
+
+    (testpath/"BUILD").write <<~EOS
+      java_binary(
+        name = "bazel-test",
+        srcs = glob(["*.java"]),
+        main_class = "ProjectRunner",
+      )
+    EOS
+
+    system bin/"bazel", "build", "//:bazel-test"
+    assert_equal "Hi!\n", pipe_output("bazel-bin/bazel-test")
+
+    # Verify that `bazel` invokes Bazel's wrapper script, which delegates to
+    # project-specific `tools/bazel` if present. Invoking `bazel-VERSION`
+    # bypasses this behavior.
+    (testpath/"tools"/"bazel").write <<~EOS
+      #!/bin/bash
+      echo "stub-wrapper"
+      exit 1
+    EOS
+    (testpath/"tools/bazel").chmod 0755
+
+    assert_equal "stub-wrapper\n", shell_output("#{bin}/bazel --version", 1)
+    assert_equal "bazel #{version}-homebrew\n", shell_output("#{bin}/bazel-#{version} --version")
+  end
+end
