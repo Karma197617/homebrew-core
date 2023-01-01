8d0bfbc53489795b0bbf5d5e730e451d527ef68c
Rui Chen <rui@chenrui.dev>
2023-01-01 00:19:27 -0500

    aws-sdk-cpp 1.10.40

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -3,8 +3,8 @@ class AwsSdkCpp < Formula
   homepage "https://github.com/aws/aws-sdk-cpp"
   # aws-sdk-cpp should only be updated every 10 releases on multiples of 10
   url "https://github.com/aws/aws-sdk-cpp.git",
-      tag:      "1.10.10",
-      revision: "9a68056cb0a7fe3f5a25ada48e3f1ce20501ce56"
+      tag:      "1.10.40",
+      revision: "3a10ae3729315f0ef5d90243a25ce61353d885e2"
   license "Apache-2.0"
   head "https://github.com/aws/aws-sdk-cpp.git", branch: "main"
 
