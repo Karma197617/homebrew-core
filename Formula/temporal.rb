a30149f715f1364de559bd519a3fbfc05f0f908e
Rui Chen <rui@chenrui.dev>
2023-02-17 10:22:28 +0100

    temporal 0.5.0

--- Formula/temporal.rb
+++ Formula/temporal.rb
@@ -1,8 +1,8 @@
 class Temporal < Formula
   desc "Command-line interface for running and interacting with Temporal Server and UI"
   homepage "https://temporal.io/"
-  url "https://github.com/temporalio/cli/archive/v0.4.0.tar.gz"
-  sha256 "cb7980fddd2e276f263ba4ea2bb5cd4662ca3148f25118dcdb221a1672561b84"
+  url "https://github.com/temporalio/cli/archive/refs/tags/0.5.0.tar.gz"
+  sha256 "37a045a9ae2721e64ee10dc4f789cb22aaea2b182e1a19a9853fadd65b218a25"
   license "MIT"
   head "https://github.com/temporalio/cli.git", branch: "main"
 
