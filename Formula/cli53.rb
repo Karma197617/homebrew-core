a16544258f4c9988c3d5d5718d5fb20928187f25
Rui Chen <rui@chenrui.dev>
2023-02-23 15:04:49 +0100

    cli53: remove build patch

--- Formula/cli53.rb
+++ Formula/cli53.rb
@@ -18,13 +18,6 @@ class Cli53 < Formula
 
   depends_on "go" => :build
 
-  # Update AWS SDK. Fixes build on Go 1.18.
-  # https://github.com/barnybug/cli53/pull/318
-  patch do
-    url "https://github.com/barnybug/cli53/commit/c60679c9171a4f8f04d09224ac4aacc316eed849.patch?full_index=1"
-    sha256 "19842038d57cc78d738754772d4535cc59a77f9da3d00982dec533a565fa193d"
-  end
-
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/cli53"
   end
