fce3e239e531b1b18a5af42dffbb0536b75b3b92
Rui Chen <rui@chenrui.dev>
2023-02-25 00:24:59 +0100

    google-java-format 1.16.0

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -3,8 +3,8 @@ class GoogleJavaFormat < Formula
 
   desc "Reformats Java source code to comply with Google Java Style"
   homepage "https://github.com/google/google-java-format"
-  url "https://github.com/google/google-java-format/releases/download/v1.15.0/google-java-format-1.15.0-all-deps.jar"
-  sha256 "a356bb0236b29c57a3ab678f17a7b027aad603b0960c183a18f1fe322e4f38ea"
+  url "https://github.com/google/google-java-format/releases/download/v1.16.0/google-java-format-1.16.0-all-deps.jar"
+  sha256 "82819a2c5f7067712e0233661b864c1c034f6657d63b8e718b4a50e39ab028f6"
   license "Apache-2.0"
 
   bottle do
