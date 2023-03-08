08ad77f60d9aa9c46c77e786cb4dfb9e01406348
Rui Chen <rui@chenrui.dev>
2023-03-07 20:00:13 -0500

    a2ps 4.15

--- Formula/a2ps.rb
+++ Formula/a2ps.rb
@@ -1,9 +1,9 @@
 class A2ps < Formula
   desc "Any-to-PostScript filter"
   homepage "https://www.gnu.org/software/a2ps/"
-  url "https://ftp.gnu.org/gnu/a2ps/a2ps-4.14.tar.gz"
-  mirror "https://ftpmirror.gnu.org/a2ps/a2ps-4.14.tar.gz"
-  sha256 "f3ae8d3d4564a41b6e2a21f237d2f2b104f48108591e8b83497500182a3ab3a4"
+  url "https://ftp.gnu.org/gnu/a2ps/a2ps-4.15.tar.gz"
+  mirror "https://ftpmirror.gnu.org/a2ps/a2ps-4.15.tar.gz"
+  sha256 "a5adc5a9222f98448a57c6b5eb6948b72743eaf9a30c67a134df082e99c76652"
   license "GPL-3.0-or-later"
 
   bottle do
