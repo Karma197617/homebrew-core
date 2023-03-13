1c5234cf73729cf7d638ca28baf0eb9ee3a23a8d
Rui Chen <rui@chenrui.dev>
2023-03-13 00:19:54 -0400

    gfold: add libgit2 linkage test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/gfold.rb
+++ Formula/gfold.rb
@@ -29,6 +29,16 @@ class Gfold < Formula
 
     assert_match "\e[0m\e[32mclean\e[0m (master)", shell_output("#{bin}/gfold #{testpath} 2>&1")
 
+    # libgit2 linkage test to avoid using vendored one
+    # https://github.com/Homebrew/homebrew-core/pull/125393#issuecomment-1465250076
+    linkage_with_libgit2 = (bin/"gfold").dynamically_linked_libraries.any? do |dll|
+      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
+
+      File.realpath(dll) == (Formula["libgit2"].opt_lib/shared_library("libgit2")).realpath.to_s
+    end
+
+    assert linkage_with_libgit2, "No linkage with libgit2! Cargo is likely using a vendored version."
+
     assert_match "gfold #{version}", shell_output("#{bin}/gfold --version")
   end
 end
