6ab09601e4fb98b3a98a98e30be8dd3c579df28a
Rui Chen <rui@chenrui.dev>
2023-03-13 13:06:47 -0400

    lacework-cli 1.13.3

--- Formula/lacework-cli.rb
+++ Formula/lacework-cli.rb
@@ -2,8 +2,8 @@ class LaceworkCli < Formula
   desc "CLI for managing Lacework"
   homepage "https://docs.lacework.com/cli"
   url "https://github.com/lacework/go-sdk.git",
-      tag:      "v1.13.2",
-      revision: "829f36abd73a6e85757869e998516368800e63a5"
+      tag:      "v1.13.3",
+      revision: "ab00f35f0f718ea87dfe077ae1b06286b4dfce7f"
   license "Apache-2.0"
   head "https://github.com/lacework/go-sdk.git", branch: "main"
 
