404be0e5eb40874b83514558d59ee66f2ec56ed7
Rui Chen <rui@chenrui.dev>
2023-03-14 17:12:39 -0400

    dotnet@6 6.0.115

--- Formula/dotnet@6.rb
+++ Formula/dotnet@6.rb
@@ -3,8 +3,8 @@ class DotnetAT6 < Formula
   homepage "https://dotnet.microsoft.com/"
   # Source-build tag announced at https://github.com/dotnet/source-build/discussions
   url "https://github.com/dotnet/installer.git",
-      tag:      "v6.0.114",
-      revision: "346c0065dd9540261ec07e938b808833446d2a9e"
+      tag:      "v6.0.115",
+      revision: "1d8c2d93c0415677f39802a8d47c8aae7c2215a3"
   license "MIT"
 
   # https://github.com/dotnet/source-build/#support
