608f6af34c611b11dbbc211a6891cbad327bc419
Rui Chen <rui@chenrui.dev>
2023-03-20 00:28:40 -0400

    joern 1.1.1535

--- Formula/joern.rb
+++ Formula/joern.rb
@@ -1,8 +1,8 @@
 class Joern < Formula
   desc "Open-source code analysis platform based on code property graphs"
   homepage "https://joern.io/"
-  url "https://github.com/joernio/joern/archive/refs/tags/v1.1.1533.tar.gz"
-  sha256 "7d1af75dcdd5603adc19136d993217612faf99e8f380e4de2bc88ae4c2d723e9"
+  url "https://github.com/joernio/joern/archive/refs/tags/v1.1.1535.tar.gz"
+  sha256 "d8a23e9b70f176a9f397113c2bde714adc0eff1b402d19c8819a524174b0dce0"
   license "Apache-2.0"
 
   livecheck do
