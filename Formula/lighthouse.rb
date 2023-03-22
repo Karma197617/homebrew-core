a34abc009fb746943d26dfadf486c2974381e6c5
Rui Chen <rui@chenrui.dev>
2023-03-22 10:47:48 -0400

    lighthouse 4.0.0

--- Formula/lighthouse.rb
+++ Formula/lighthouse.rb
@@ -1,8 +1,8 @@
 class Lighthouse < Formula
   desc "Rust Ethereum 2.0 Client"
   homepage "https://github.com/sigp/lighthouse"
-  url "https://github.com/sigp/lighthouse/archive/refs/tags/v3.5.1.tar.gz"
-  sha256 "5e5c7a5f9ac4dd7d079f43a0e872ba80ed647457a4008be2a61e061cc0226aee"
+  url "https://github.com/sigp/lighthouse/archive/refs/tags/v4.0.0.tar.gz"
+  sha256 "54212e01ab3c6a0d426d94fc7c02d0c442c769caa8fe3a57a015c40a3ac0784d"
   license "Apache-2.0"
 
   bottle do
