fb93a5dce26cfd5e6634443bdab92a8798605f71
Rui Chen <rui@chenrui.dev>
2023-04-24 18:38:49 +0800

    apache-pulsar 2.11.1

--- Formula/apache-pulsar.rb
+++ Formula/apache-pulsar.rb
@@ -1,9 +1,9 @@
 class ApachePulsar < Formula
   desc "Cloud-native distributed messaging and streaming platform"
   homepage "https://pulsar.apache.org/"
-  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/pulsar-2.10.3/apache-pulsar-2.10.3-src.tar.gz"
-  mirror "https://archive.apache.org/dist/pulsar/pulsar-2.10.3/apache-pulsar-2.10.3-src.tar.gz"
-  sha256 "4fca38025c6059b0cb1b8c8ca7526a6c525769529c270a0172e2294d311b8f96"
+  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/pulsar-2.11.1/apache-pulsar-2.11.1-src.tar.gz"
+  mirror "https://archive.apache.org/dist/pulsar/pulsar-2.11.1/apache-pulsar-2.11.1-src.tar.gz"
+  sha256 "0ccd3969162ce1cd99efe11d9b0ccb2c2bd101f578c66a4d099b060a0b603f98"
   license "Apache-2.0"
   head "https://github.com/apache/pulsar.git", branch: "master"
 
