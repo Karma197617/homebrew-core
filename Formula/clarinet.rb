b73460ff6d261ed9a06191a095c48e5c5c1f0dce
Rui Chen <rui@chenrui.dev>
2023-05-14 13:32:02 -0400

    clarinet 1.6.0

--- Formula/clarinet.rb
+++ Formula/clarinet.rb
@@ -3,8 +3,8 @@ class Clarinet < Formula
   homepage "https://github.com/hirosystems/clarinet"
   # pull from git tag to get submodules
   url "https://github.com/hirosystems/clarinet.git",
-      tag:      "v1.5.4",
-      revision: "3b741691b0417d06f03b3a83935dc13808363d4c"
+      tag:      "v1.6.0",
+      revision: "f2d3f9c5a3a10d8148c50cfec7916a83b236f02d"
   license "GPL-3.0-only"
   head "https://github.com/hirosystems/clarinet.git", branch: "main"
 
