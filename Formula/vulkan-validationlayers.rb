8ae4909d7b9d21fd7d6e6f841ff8e3ffdb0d16b5
Rui Chen <rui@chenrui.dev>
2023-05-31 18:38:45 -0400

    vulkan-validationlayers 1.3.251
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    vulkan-validationlayers: fix
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/vulkan-validationlayers.rb
+++ Formula/vulkan-validationlayers.rb
@@ -5,8 +5,8 @@ class VulkanValidationlayers < Formula
   head "https://github.com/KhronosGroup/Vulkan-ValidationLayers.git", branch: "main"
 
   stable do
-    url "https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/v1.3.250.tar.gz"
-    sha256 "1c3609321c1167f9af5d3687a443885e2cb1e8e5150df16356200e84bef685f3"
+    url "https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/v1.3.251.tar.gz"
+    sha256 "629608cd549df6ba8ae581667dd0506d9fb67aa1b8c8a210f590c8cb76e34fae"
 
     # upstream commit ref, https://github.com/KhronosGroup/SPIRV-Tools/commit/d4c0abdcad60325a2ab3c00a81847e2dbdc927a2
     # remove in next release
