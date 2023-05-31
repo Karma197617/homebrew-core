394c838c1fa1e39530d30cd18ed5addff105e996
Rui Chen <rui@chenrui.dev>
2023-05-31 15:18:26 -0400

    grpc: revision bump (re2 20230601)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -5,6 +5,7 @@ class Grpc < Formula
       tag:      "v1.54.2",
       revision: "8871dab19b4ab5389e28474d25cfeea61283265c"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/grpc/grpc.git", branch: "master"
 
   # The "latest" release on GitHub is sometimes for an older major/minor and
